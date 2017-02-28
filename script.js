6f5079fe579c13a4997d2799d51b6065cafa0b08
vanessamuller <vane9090@gmail.com>
2017-02-28 18:53:04 -0500

    set up ajax api call

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $('#search').click(function(){
+
+    var searchTerm = $('#searchTerm').val();
+    var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback=?"
+
+
+
+    $.ajax({
+      type:"GET",
+      url:url,
+      async:false,
+      dataType:"json",
+      success: function(data){
+        console.log(data[1][0]);
+        console.log(data[2][0]);
+        console.log(data[3][0]);
+      },
+      error:function(errorMessage){
+        alert("Error");
+      }
+    });
+  });
+});
